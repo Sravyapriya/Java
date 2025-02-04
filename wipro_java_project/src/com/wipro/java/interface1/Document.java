43b801b4963f01b674107c7c5cafdf805aba6697
Sravyapriya <sravya99991@gmail.com>
2025-02-04 18:14:50 +0530

    commit day3

--- wipro_java_project/src/com/wipro/java/interface1/Document.java
+++ wipro_java_project/src/com/wipro/java/interface1/Document.java
@@ -12,13 +12,10 @@ public class Document implements Showable , Printable{
 		
 		System.out.println("show");
 	}
-
 	public static void main(String a[]) {
 		// TODO Auto-generated method stub
 		Document doc1= new Document();
 		doc1.show();
 		doc1.print();
-
 	}
-
 }
