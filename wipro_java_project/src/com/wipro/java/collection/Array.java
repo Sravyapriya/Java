6545a6463ea4cc37a0151d7f51736c61442620a3
Sravyapriya <sravya99991@gmail.com>
2025-02-05 15:02:00 +0530

    commit day4

--- wipro_java_project/src/com/wipro/java/collection/Array.java
+++ wipro_java_project/src/com/wipro/java/collection/Array.java
@@ -6,13 +6,16 @@ class Array {
 	
 	arr = new int[5];
 	
+	//initializing the 1st elements of array
 	arr[0] = 10;
+	
 	arr[1] = 20;
 	
 	arr[2] = 30;
 	arr[3] = 40;
 	arr[5] = 50;
 	
+	//accessing the elements of a specified array
 	for(int i=0;i<arr.length;i++)
 		System.out.println("Element at index " + i + " : " + arr[i]);
  	}
