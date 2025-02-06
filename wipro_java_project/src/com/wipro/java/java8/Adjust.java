7f083c42744763a735db3468065e7aec9b4c892d
Sravyapriya <sravya99991@gmail.com>
2025-02-07 01:22:50 +0530

    commit day5

---
+++ wipro_java_project/src/com/wipro/java/java8/Adjust.java
@@ -0,0 +1,47 @@
+package com.wipro.java.java8;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
+
+public class Adjust  {
+	
+	public static void checkingAdjusters()
+	{
+		LocalDate date = LocalDate.now();
+		System.out.println("the current date is "+ 
+		                    date);
+		
+		LocalDate dayOfNextMonth = 
+				date.with(TemporalAdjusters.
+						firstDayOfNextMonth());
+		
+		System.out.println("firstDayOfNextMonth: " +
+		                    dayOfNextMonth);
+		
+		LocalDate nextSaturday =
+				date.with(TemporalAdjusters.
+						next(DayOfWeek.SATURDAY));
+		
+		System.out.println("next saturday from now is "+ 
+		                    nextSaturday);
+		
+		LocalDate firstDay = 
+				date.with(TemporalAdjusters.
+						firstDayOfMonth());
+		System.out.println("firstDayOfMonth : " +
+						firstDay);
+		
+		LocalDate lastDay = 
+				date.with(TemporalAdjusters.
+						lastDayOfMonth());
+		
+		System.out.println("lastDayOfMonth : " +
+		                   lastDay);
+
+	public static void main(String[] args) {
+		 checkingAdjusters();
+
+	}
+}
+}
